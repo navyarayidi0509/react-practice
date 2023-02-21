23c9810ea87d5684242663f25e905ddd389c61a1
LAPTOP-GSL4SKDE\rayid <rayidinavya@gmail.com>
2023-02-21 14:28:29 +0530

    new ex file created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import P from './components/Functionc' ;
+import P from './components/Function1' ;
 class App extends Component{
   render(){
   return (
---
+++ src/components/Function1.js
@@ -0,0 +1,32 @@
+import React from "react";
+function P(){
+    return (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>id</th>
+              <th>role</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>navya</td>
+              <td>11</td>
+              <td>software</td>
+            </tr>
+            <tr>
+              <td>sita</td>
+              <td>12</td>
+              <td>engineer</td>
+            </tr>
+            <tr>
+              <td>sre</td>
+              <td>13</td>
+              <td>tester</td>
+            </tr>
+          </tbody>
+        </table>
+    )
+}
+export default P;
